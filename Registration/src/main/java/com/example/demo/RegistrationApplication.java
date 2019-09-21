fb4744d24bf5b3d6260e05e94e3babc53fac71c9
SAMAPTY-SAHA <41911451+SAMAPTY-SAHA@users.noreply.github.com>
2019-09-21 17:24:04 +0600

    change

--- Registration/src/main/java/com/example/demo/RegistrationApplication.java
+++ Registration/src/main/java/com/example/demo/RegistrationApplication.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
+/*@EnableAutoConfiguration(exclude = {
+		 ErrorMvcAutoConfiguration.class
+		})*/
 public class RegistrationApplication {
 
 	public static void main(String[] args) {
